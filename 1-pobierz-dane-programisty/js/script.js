0df212ac02ff50ff879beb31f50e9edc9e96b78e
Kasia Goj <katarzyna.goj@fundingbox.com>
2020-11-14 17:43:14 +0100

    programmer data in JSON format retrieved

--- 1-pobierz-dane-programisty/js/script.js
+++ 1-pobierz-dane-programisty/js/script.js
@@ -1,47 +1,43 @@
-/* Adres URL do API na serwerze: https://akademia108.pl/api/ajax/get-post.php
+/* Stwórz stronę zawierającą przycisk pt. "Pobierz dane"
 
-Po pobraniu danych w formacie JSON wyciąg z nich:
+Po kliknięciu przycisku pobierz dane o programiście w formacie JSON z internetu za pomocą Javascript lub jQuery:
 
-userId
-id
-title
-body */
+https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php
+imię, nazwisko, zawód firma
+Pobrane dane wyświetl poniżej przycisku w nowym, stworzonym DIVie o id="dane-programisty".
+
+Zadanie domowe zrób za pomocą GitHub Pages. */
 
 let btnGetData = document.getElementById('get-data');
 
-console.log(btnGetData);
+// console.log(btnGetData);
 
 const getData = () => {
-    fetch('https://akademia108.pl/api/ajax/get-post.php')
+    fetch('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
 
     .then(response => response.json())
     .then((data) => {
 
-        let pUserId = document.createElement('p');
-        let pId = document.createElement('p');
-        let pTitle = document.createElement('p');
-        let pBody = document.createElement('p');
+        let pName = document.createElement('p');
+        let pLastName = document.createElement('p');
+        let pJob = document.createElement('p');
+        let pCompany = document.createElement('p');
 
-        pUserId.innerText = `User ID: ${data.userId}`
-        pId.innerText = `ID: ${data.id}`
-        pTitle.innerText = `Title: ${data.title}`
-        pBody.innerText = `Body: ${data.body}`
+        pName.innerText = `Imię: ${data.imie}`
+        pLastName.innerText = `Nazwisko: ${data.nazwisko}`
+        pJob.innerText = `Zawód: ${data.zawod}`
+        pCompany.innerText = `Firma: ${data.firma}`
 
-        $('#dane-programisty').append(pUserId);
-        $('#dane-programisty').append(pId);
-        $('#dane-programisty').append(pTitle);
-        $('#dane-programisty').append(pBody);
-
-        // document.body.appendChild(pUserId);
-        // document.body.appendChild(pId);
-        // document.body.appendChild(pTitle);
-        // document.body.appendChild(pBody);
+        $('#dane-programisty').append(pName);
+        $('#dane-programisty').append(pLastName);
+        $('#dane-programisty').append(pJob);
+        $('#dane-programisty').append(pCompany);
 
         // console.log(data);
-        console.log(data.userId);
-        console.log(data.id);
-        console.log(data.title);
-        console.log(data.body);
+        // console.log(data.imie);
+        // console.log(data.nazwisko);
+        // console.log(data.zawod);
+        // console.log(data.firma);
     })
     .catch(error => {
         console.error(error);
